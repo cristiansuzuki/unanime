7c485625d7c1c7644a809478dd832fde275569cb
Cristian Suzuki <cristiansuzuki5@hotmail.com>
2026-01-14 11:17:44 -0500

    alteracoes nas permissoes dos usuarios

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sm!0&3igh4x9k@&^7k=me2zb8-1rl-72!aw(avu4y04*!4whmx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '127.0.0.1', '192.168.0.241']
 
 
 # Application definition
